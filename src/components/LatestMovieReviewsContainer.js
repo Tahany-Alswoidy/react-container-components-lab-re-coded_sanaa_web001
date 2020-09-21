20958022641c8ddc1ded5d26e004d2c1a18719f7
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-21 02:16:18 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+export default class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super();
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+    this.state = {
+      reviews: []
+    };
+  }
 
-// Code LatestMovieReviewsContainer Here
+  componentDidMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(json => this.setState({ reviews: json.results }));
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
